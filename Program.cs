2ae549f039d9e3f069a7a916eccca544610d755c
Harper <154289487+O5Desmos@users.noreply.github.com>
2025-03-02 16:30:35 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+
+Console.WriteLine("Hey lol");
+
+const string userRoot = "HKEY_CURRENT_USER";
+const string subkey = "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
+const string keyName = userRoot + "\\" + subkey;
+
+Registry.SetValue(keyName, "AppsUseLightTheme",0, RegistryValueKind.DWord);
+Console.WriteLine("\"AppsUseLightTheme\" is now false");
+
+Registry.SetValue(keyName, "SystemUsesLightTheme",0, RegistryValueKind.DWord);
+Console.WriteLine("\"SystemUsesLightTheme\" is now false");
+
+Console.WriteLine("You're Welcome (press any key to exit.)");
+Console.ReadKey();
+
